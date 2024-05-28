909432199f21559cb03ca2e72f0d9fc57407a966
yann <yannpickern@gmail.com>
2024-05-28 19:41:42 +0200

    Initial commit

---
+++ src/main/java/net/splintery/qwak/QwakClient.java
@@ -0,0 +1,10 @@
+package net.splintery.qwak;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QwakClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/splintery/qwak/QwakDataGenerator.java
@@ -0,0 +1,11 @@
+package net.splintery.qwak;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class QwakDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
